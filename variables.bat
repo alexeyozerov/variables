aa8bfbd98ce29e23d5f0d2127ff3a48029bdaa9b
alexey.ozerov <alexey.ozerov@antilatency.com>
2018-03-02 17:24:09 +0300

    fix ""

--- variables.bat
+++ variables.bat
@@ -16,12 +16,12 @@ set "TEMP_WORK_DIR=C:\Users\Alex\Documents"
 		
 			set "TARGET_EXE=C:\Users\Alex\Documents\TEMPresultqmake\release\test.exe"
 	
-set "ENVIRONMENT_X64=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+set ENVIRONMENT_X64="%C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat%"
 
 set "QMAKE_EXE=D:\Qt\5.10.0\msvc2017_64\bin\qmake.exe"
 
 set "WIN_DEPLOY=D:\Qt\5.10.0\msvc2017_64\bin\windeployqt.exe"
 
-set "ZIP_ROOT=C:\Program Files\7-Zip"
+set ZIP_ROOT="%C:\Program Files\7-Zip%"
 
 set "BACKUP_ZIP=D:\backup_builds\build%system.build.number%.zip"
