8a15fa0b92e92b24e742dcdf6a11dac9164a42f5
alexey.ozerov <alexey.ozerov@antilatency.com>
2018-03-02 17:19:11 +0300

    updated variables

--- variables.bat
+++ variables.bat
@@ -1,14 +1,27 @@
-set TEMP_WORK_DIR="%C:\Users\Alex\Documents%"
-set TEMP_RESULT_QMAKE="%C:\Users\Alex\Documents\TEMPresultqmake%"
-set ENVIRONMENT_X64="%C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat%"
+set "TEMP_WORK_DIR=C:\Users\Alex\Documents"
+
+	set "PROJECT_PATH=C:\Users\Alex\Documents\test"
+
+		set "PRO_FILE=C:\Users\Alex\Documents\test\test.pro"
+	
+	set "TEMP_RESULT_QMAKE=C:\Users\Alex\Documents\TEMPresultqmake"
+	
+		set "TEMP_BUILD=C:\Users\Alex\Documents\TEMPresultqmake\TEMPbuild"
+		
+			set "TESTING_EXE=C:\Users\Alex\Documents\TEMPresultqmake\TEMPbuild\test.exe"
+			
+			set "RESULT_ZIP=C:\Users\Alex\Documents\TEMPresultqmake\TEMPbuild.zip"
+	
+		set "TEMP_RESULT_QMAKE_SUBFOLDER=C:\Users\Alex\Documents\TEMPresultqmake\release"
+		
+			set "TARGET_EXE=C:\Users\Alex\Documents\TEMPresultqmake\release\test.exe"
+	
+set "ENVIRONMENT_X64=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+
 set "QMAKE_EXE=D:\Qt\5.10.0\msvc2017_64\bin\qmake.exe"
-set "PRO_FILE=C:\Users\Alex\Documents\test\test.pro"
-set TEMP_RESULT_QMAKE_SUBFOLDER="%C:\Users\Alex\Documents\TEMPresultqmake\release%"
-set "TARGET_EXE=C:\Users\Alex\Documents\TEMPresultqmake\release\test.exe"
-set TEMP_BUILD="%C:\Users\Alex\Documents\TEMPresultqmake\TEMPbuild%"
+
 set "WIN_DEPLOY=D:\Qt\5.10.0\msvc2017_64\bin\windeployqt.exe"
-set PROJECT_PATH="%C:\Users\Alex\Documents\test%"
-set "TESTING_EXE=C:\Users\Alex\Documents\TEMPresultqmake\TEMPbuild\test.exe"
-set ZIP_ROOT="%C:\Program Files\7-Zip\%"
-set "RESULT_ZIP=C:\Users\Alex\Documents\TEMPresultqmake\TEMPbuild.zip"
+
+set "ZIP_ROOT=C:\Program Files\7-Zip"
+
 set "BACKUP_ZIP=D:\backup_builds\build%system.build.number%.zip"
--- variables.ps1
+++ variables.ps1
@@ -1,14 +1,27 @@
 $TEMP_WORK_DIR="C:\Users\Alex\Documents"
-$TEMP_RESULT_QMAKE="C:\Users\Alex\Documents\TEMPresultqmake"
+
+	$PROJECT_PATH="C:\Users\Alex\Documents\test"
+
+		$PRO_FILE="C:\Users\Alex\Documents\test\test.pro"
+	
+	$TEMP_RESULT_QMAKE="C:\Users\Alex\Documents\TEMPresultqmake"
+	
+		$TEMP_BUILD="C:\Users\Alex\Documents\TEMPresultqmake\TEMPbuild"
+		
+			$TESTING_EXE="C:\Users\Alex\Documents\TEMPresultqmake\TEMPbuild\test.exe"
+			
+			$RESULT_ZIP="C:\Users\Alex\Documents\TEMPresultqmake\TEMPbuild.zip"
+	
+		$TEMP_RESULT_QMAKE_SUBFOLDER="C:\Users\Alex\Documents\TEMPresultqmake\release"
+		
+			$TARGET_EXE="C:\Users\Alex\Documents\TEMPresultqmake\release\test.exe"
+	
 $ENVIRONMENT_X64="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+
 $QMAKE_EXE="D:\Qt\5.10.0\msvc2017_64\bin\qmake.exe"
-$PRO_FILE="C:\Users\Alex\Documents\test\test.pro"
-$TEMP_RESULT_QMAKE_SUBFOLDER="C:\Users\Alex\Documents\TEMPresultqmake\release"
-$TARGET_EXE="C:\Users\Alex\Documents\TEMPresultqmake\release\test.exe"
-$TEMP_BUILD="C:\Users\Alex\Documents\TEMPresultqmake\TEMPbuild"
+
 $WIN_DEPLOY="D:\Qt\5.10.0\msvc2017_64\bin\windeployqt.exe"
-$PROJECT_PATH="C:\Users\Alex\Documents\test"
-$TESTING_EXE="C:\Users\Alex\Documents\TEMPresultqmake\TEMPbuild\test.exe"
-$ZIP_ROOT="C:\Program Files\7-Zip\"
-$RESULT_ZIP="C:\Users\Alex\Documents\TEMPresultqmake\TEMPbuild.zip"
+
+$ZIP_ROOT="C:\Program Files\7-Zip"
+
 $BACKUP_ZIP="D:\backup_builds\build%system.build.number%.zip"
